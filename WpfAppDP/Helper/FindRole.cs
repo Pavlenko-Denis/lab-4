93ddb3886a4a0bd4f280a16cdf379fb3cd382956
Pavlenko-Denis <tepen2000@mail.ru>
2021-12-27 07:18:20 +0300

    lab-4
    test

---
+++ WpfAppDP/Helper/FindRole.cs
@@ -0,0 +1,23 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppDP.Model;
+
+namespace WpfAppDP.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+
+}
---
+++ WpfAppDP/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Windows;
+using WpfAppDP.Model;
+using WpfAppDP.View;
+
+namespace WpfAppDP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RoleViewModel vmRole = new RoleViewModel();
+        private PersonViewModel vmPerson = new PersonViewModel();
+
+
+        public static int IdRole { get; set; }
+        public static int IdEmployee { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+             private void Employee_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowEmployee wEmployee = new WindowEmployee(vmPerson, vmRole);
+            wEmployee.Title = "Сотрудники";
+            wEmployee.Show();
+        }
+        private void Role_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowRole wRole = new WindowRole(vmRole);
+            wRole.Show();
+        }
+
+    }
+}
+
